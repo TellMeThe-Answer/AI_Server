bad1f40da0d5ba247ff4994b7d6a7305ed9003bc
rong5026 <rong5026@naver.com>
2023-10-14 14:50:33 +0900

    [TEST] 이미지 파일받아 저장

--- app.py
+++ app.py
@@ -12,9 +12,11 @@ sys.path.insert(0, './model')
 # 모델 로딩
 model = torch.hub.load('./yolov5', 'custom', path='./model/best_model.pt', source='local')
 
+# 이미지 저장
 def save_image(file):
     file.save('./img/'+ file.filename)
     
+
 @app.route('/')
 def hello():
     return 'Hello World!'
@@ -22,17 +24,17 @@ def hello():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['image_file']
         save_image(file) # 들어오는 이미지 저장
-        train_img = './img/' + file.filename
-        temp = model(train_img)
-        # 결과 출력
-        temp.print()
+        # train_img = './img/' + file.filename
+        # temp = model(train_img)
+        # # 결과 출력
+        # temp.print()
 
-        # 결과 이미지 저장
-        temp.save()  # save results (image with detections)
+        # # 결과 이미지 저장
+        # temp.save()  # save results (image with detections)
 
-        return 1
+        return "sdf"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
